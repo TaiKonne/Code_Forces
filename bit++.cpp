5ebf210dd88866b3e0d8a511fe4addc5512d3158
TaiKonne <2124802010067@student.tdmu.edu.vn>
2022-05-21 00:39:40 +0700

    comit

--- bit++.cpp
+++ bit++.cpp
@@ -5,22 +5,19 @@ int main()
 {
     int n;
     cin >> n;
-    vector<string> a;
+
+    int count=0;
     for (int i = 0; i < n; i++)
     {
         string t;
         cin >> t;
-        a.push_back(t);
-    }
-    int count=0;
-    for (int i = 0; i < a.size(); i++)
-    {
-        if (a[i] == "X++"||a[i]=="++X")
+        if (t == "X++" || t == "++X")
         {
             count++;
         }
         else
             count--;
     }
-    cout<<count;
+
+    cout << count;
 }
